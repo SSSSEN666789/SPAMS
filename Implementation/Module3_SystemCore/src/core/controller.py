5d6ebfb30eeab55ab73eead2363e8a802ad9b540
ChoiSeongJin <nature9545@gmail.com>
2021-05-31 14:02:03 +0900

    Module3-System_core-EQ_cont

--- Implementation/Module3_SystemCore/src/core/controller.py
+++ Implementation/Module3_SystemCore/src/core/controller.py
@@ -5,10 +5,18 @@
 #근데 report가 원래는 save에 인자로 들어가야 하는데 없어서 not accessed Paylance가 뜸
 #testResult에 True와 False를 저장시켜 notify함수 실행시켜 안내메세지 출력함
 
+#main.py 참조해서 testrunner와 testresultprocessor 인스턴스 생성 후
+#함수 호출하고, strategy 패턴 삽입
+
 from .test_runner import TestRunner
 from .test_result_processor import TestResultProcessor
 from .evaluation_queue import dequeue
+from .python_test_strategy import PythonTestStrategy
+from .plain_text_test_strategy import PlainTextTestStrategy
+from .python_report_render_strategy import PythonReportRenderStrategy
+from .plain_text_report_render_strategy import PlainTextReportRenderStrategy
 from Implementation.Module3_SystemCore.src.core import test_result_processor
+
 
 
 class contorller():
@@ -21,17 +29,33 @@ class contorller():
 
     def autoGrade(self):
         while True:
-            evalReq = dequeue()
-            if evalReq is not None:
-                break
-        result = TestRunner.runTest(evalReq.language)
-        if result is not None:
-            report = TestResultProcessor.makeReport(result)
-            testResult = True
-        else:
-            print("runTest Failed")
-            testResult = False
-
-
-        self.notify(testResult)
+            while True:
+                evalReq = dequeue()
+                if evalReq is not None:
+                    break
+            if evalReq.language == "python":
+                t_strategy = PythonTestStrategy()
+                runner = TestRunner(strategy=t_strategy)
+                result = runner.runTest()
+                if result is not None:
+                    r_strategy = PythonReportRenderStrategy()
+                    processor = TestResultProcessor(strategy=r_strategy)
+                    report = processor.makeReport(result)
+                    testResult = True
+                else:
+                    print("runTest Failed")
+                    testResult = False
+            elif evalReq.language == "plaintext":
+                t_strategy = PlainTextTestStrategy()
+                runner = TestRunner(strategy=t_strategy)
+                result = runner.runTest()
+                if result is not None:
+                    r_strategy = PlainTextReportRenderStrategy()
+                    processor = TestResultProcessor(strategy=r_strategy)
+                    report = processor.makeReport(result)
+                    testResult = True
+                else:
+                    print("runTest Failed")
+                    testResult = False
+            self.notify(testResult)
 
